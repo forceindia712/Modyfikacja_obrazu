330166244213b4e1b285d6f7970a9b5139d19a23
forceindia712 <80550704+forceindia712@users.noreply.github.com>
2021-05-03 19:21:17 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿
+namespace Grafika_Lab7
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.trackBar2);
+            this.panel1.Controls.Add(this.trackBar1);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1793, 835);
+            this.panel1.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(837, 288);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(114, 26);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Filtr Gaussa";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(837, 244);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 26);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Filtr rozmywający";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(861, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 30);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Wczytaj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.Control;
+            this.panel3.Location = new System.Drawing.Point(1299, 630);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(256, 180);
+            this.panel3.TabIndex = 5;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Control;
+            this.panel2.Location = new System.Drawing.Point(243, 630);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(256, 180);
+            this.panel2.TabIndex = 4;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(822, 172);
+            this.trackBar2.Maximum = 255;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(153, 45);
+            this.trackBar2.TabIndex = 3;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(822, 94);
+            this.trackBar1.Maximum = 255;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(153, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pictureBox2.Location = new System.Drawing.Point(993, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(800, 600);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 600);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "Wczytaj obraz";
+            this.openFileDialog1.Filter = "Pliki jpg|*.jpg|Wszystkie pliki|*.*";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1793, 835);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,475 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grafika_Lab7
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bitmap;
+        int wys, szer;
+
+        int[] R = new int[256];
+        int[] G = new int[256];
+        int[] B = new int[256];
+
+        int[] H = new int[256];
+
+        int K = 1;
+
+        double[,] macierz;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void histogram()
+        {
+            for (int y = 0; y < wys; y++)
+            {
+                for (int x = 0; x < szer; x++)
+                {
+                    Color c = bitmap.GetPixel(x, y);
+
+                    int r = c.R;
+                    int g = c.G;
+                    int b = c.B;
+
+                    R[r]++;
+                    G[g]++;
+                    B[b]++;
+                }
+            }
+
+            for (int i = 0; i < H.Length; i++)
+            {
+                H[i] = R[i] + B[i] + G[i];
+            }
+        }
+
+        private void histogram2(Bitmap bitmapEdited)
+        {
+            for (int y = 0; y < wys; y++)
+            {
+                for (int x = 0; x < szer; x++)
+                {
+                    Color c = bitmapEdited.GetPixel(x, y);
+
+                    int r = c.R;
+                    int g = c.G;
+                    int b = c.B;
+
+                    R[r]++;
+                    G[g]++;
+                    B[b]++;
+                }
+            }
+
+            for (int i = 0; i < H.Length; i++)
+            {
+                H[i] = R[i] + B[i] + G[i];
+            }
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            if (K == 0)
+            {
+                Graphics graphR = e.Graphics;
+
+                for (int i = 0; i < 256; i++)
+                {
+                    float s = H[i];
+
+                    s = s / (pictureBox1.Image.Height * pictureBox1.Image.Width);
+                    s *= 1700;
+
+                    graphR.DrawLine(new Pen(Color.Black), i, panel2.Height, i, panel2.Height - s);
+                }
+            }
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+            if (K == 0)
+            {
+                Graphics graphR = e.Graphics;
+
+                for (int i = 0; i < 256; i++)
+                {
+                    float s = H[i];
+
+                    s = s / (pictureBox1.Image.Height * pictureBox1.Image.Width);
+                    s *= 1700;
+
+                    graphR.DrawLine(new Pen(Color.Black), i, panel2.Height, i, panel2.Height - s);
+                }
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            Bitmap bitmapEdited = (Bitmap)bitmap.Clone();
+            Bitmap bitmapCopy = (Bitmap)bitmap.Clone();
+
+            for (int y = 0; y < wys; y++)
+            {
+                for (int x = 0; x < szer; x++)
+                {
+                    Color c = bitmapCopy.GetPixel(x, y);
+
+                    double r = (double)c.R;
+                    double g = (double)c.G;
+                    double b = (double)c.B;
+
+                    int c1 = 102;
+                    int d = 230;
+
+
+                    r = (r - trackBar1.Value) * (d - c1) / (trackBar2.Value - trackBar1.Value) + c1;
+                    g = (g - trackBar1.Value) * (d - c1) / (trackBar2.Value - trackBar1.Value) + c1;
+                    b = (b - trackBar1.Value) * (d - c1) / (trackBar2.Value - trackBar1.Value) + c1;
+
+
+                    if (r < 0)
+                    {
+                        r = 0;
+                    }
+                    if (g < 0)
+                    {
+                        g = 0;
+                    }
+                    if (b < 0)
+                    {
+                        b = 0;
+                    }
+                    if (r > 255)
+                    {
+                        r = 255;
+                    }
+                    if (g > 255)
+                    {
+                        g = 255;
+                    }
+                    if (b > 255)
+                    {
+                        b = 255;
+                    }
+
+
+                    bitmapEdited.SetPixel(x, y, Color.FromArgb((int)r, (int)g, (int)b));
+                }
+            }
+
+            pictureBox2.Image = bitmapEdited;
+
+            Array.Clear(R, 0, R.Length);
+            Array.Clear(G, 0, G.Length);
+            Array.Clear(B, 0, B.Length);
+
+
+            histogram2(bitmapEdited);
+            K = 0;
+            panel3.Invalidate();
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            Bitmap bitmapEdited = (Bitmap)bitmap.Clone();
+            Bitmap bitmapCopy = (Bitmap)bitmap.Clone();
+
+
+            int width = pictureBox1.Image.Width;
+            int height = pictureBox1.Image.Height;
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Color c = bitmapCopy.GetPixel(x, y);
+
+                    double r = (double)c.R;
+                    double g = (double)c.G;
+                    double b = (double)c.B;
+
+                    int c1 = 102;
+                    int d = 230;
+
+
+                    r = (r - trackBar1.Value) * (d - c1) / (trackBar2.Value - trackBar1.Value) + c1;
+                    g = (g - trackBar1.Value) * (d - c1) / (trackBar2.Value - trackBar1.Value) + c1;
+                    b = (b - trackBar1.Value) * (d - c1) / (trackBar2.Value - trackBar1.Value) + c1;
+
+                    if (r < 0)
+                    {
+                        r = 0;
+                    }
+                    if (g < 0)
+                    {
+                        g = 0;
+                    }
+                    if (b < 0)
+                    {
+                        b = 0;
+                    }
+                    if (r > 255)
+                    {
+                        r = 255;
+                    }
+                    if (g > 255)
+                    {
+                        g = 255;
+                    }
+                    if (b > 255)
+                    {
+                        b = 255;
+                    }
+
+                    bitmapEdited.SetPixel(x, y, Color.FromArgb((int)r, (int)g, (int)b));
+                }
+            }
+
+            pictureBox2.Image = bitmapEdited;
+
+
+            Array.Clear(R, 0, R.Length);
+            Array.Clear(G, 0, G.Length);
+            Array.Clear(B, 0, B.Length);
+
+
+            histogram2(bitmapEdited);
+
+            K = 0;
+            panel3.Invalidate();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap bitmapEdited = (Bitmap)bitmap.Clone();
+            Bitmap bitmapCopy = (Bitmap)bitmap.Clone();
+
+            double pomocR = 0, pomocG = 0, pomocB = 0;
+
+            double[,] macierz = new double[,] { { 1, 1, 1 }, { 1, 1, 1 }, { 1, 1, 1 } };
+
+
+            for (int i = 0; i < wys; i++)
+            {
+                for (int j = 0; j < szer; j++)
+                {
+
+                    pomocR = 0; pomocG = 0; pomocB = 0;
+
+                    for (int k = -1; k <= 1; k++)
+                    {
+                        for (int l = -1; l <= 1; l++)
+                        {
+                            int x1 = i + k;
+                            int y1 = j + l;
+
+                            if (x1 < 0)
+                            {
+                                x1 = 0;
+                            }
+                            if (x1 >= bitmap.Height)
+                            {
+                                x1 = bitmap.Height - 1;
+                            }
+                            if (y1 < 0)
+                            {
+                                y1 = 0;
+                            }
+                            if (y1 >= bitmap.Width)
+                            {
+                                y1 = bitmap.Width - 1;
+                            }
+                            Color p = bitmapCopy.GetPixel(y1, x1);
+
+                            double r = (double)p.R;
+                            double g = (double)p.G;
+                            double b = (double)p.B;
+
+                            pomocR += r * macierz[k + 1, l + 1];
+                            pomocG += g * macierz[k + 1, l + 1];
+                            pomocB += b * macierz[k + 1, l + 1];
+
+                        }
+                    }
+
+                    pomocR = pomocR / 9;
+                    pomocG = pomocG / 9;
+                    pomocB = pomocB / 9;
+
+
+                    if (pomocR < 0)
+                    {
+                        pomocR = 0;
+                    }
+                    if (pomocG < 0)
+                    {
+                        pomocG = 0;
+                    }
+                    if (pomocB < 0)
+                    {
+                        pomocB = 0;
+                    }
+                    if (pomocR > 255)
+                    {
+                        pomocR = 255;
+                    }
+                    if (pomocG > 255)
+                    {
+                        pomocG = 255;
+                    }
+                    if (pomocB > 255)
+                    {
+                        pomocB = 255;
+                    }
+
+                    bitmapEdited.SetPixel(j, i, Color.FromArgb((int)pomocR, (int)pomocG, (int)pomocB));
+                }
+            }
+
+            pictureBox2.Image = bitmapEdited;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int k = 7;
+            int r1 = 15;
+            int sigma = 10;
+            double suma = 0;
+
+            macierz = new double[r1, r1];
+
+            for (int i = 0; i < 15; i++)
+            {
+                for (int j = 0; j < 15; j++)
+                {
+                    macierz[i, j] = (1 / (2 * Math.PI * Math.Pow(sigma, 2))) * Math.Pow(Math.E, -(Math.Pow(i - k, 2) + Math.Pow(j - k, 2)) / (2 * Math.Pow(sigma, 2)));
+                    suma = suma + macierz[i, j];
+
+                    Debug.WriteLine(macierz[i, j]);
+                }
+            }
+
+            Bitmap bitmapEdited = (Bitmap)bitmap.Clone();
+            Bitmap bitmapCopy = (Bitmap)bitmap.Clone();
+
+            double pomocR = 0, pomocG = 0, pomocB = 0;
+
+            for (int i = 0; i < wys; i++)
+            {
+                for (int j = 0; j < szer; j++)
+                {
+
+                    pomocR = 0; pomocG = 0; pomocB = 0;
+
+                    for (int f = -7; f <= 7; f++)
+                    {
+                        for (int l = -7; l <= 7; l++)
+                        {
+                            int x1 = i + f;
+                            int y1 = j + l;
+
+                            if (x1 < 0)
+                            {
+                                x1 = 0;
+                            }
+                            if (x1 >= bitmap.Height)
+                            {
+                                x1 = bitmap.Height - 1;
+                            }
+                            if (y1 < 0)
+                            {
+                                y1 = 0;
+                            }
+                            if (y1 >= bitmap.Width)
+                            {
+                                y1 = bitmap.Width - 1;
+                            }
+                            Color p = bitmapCopy.GetPixel(y1, x1);
+
+                            double r = (double)p.R;
+                            double g = (double)p.G;
+                            double b = (double)p.B;
+
+                            pomocR += r * macierz[f + k, l + k];
+                            pomocG += g * macierz[f + k, l + k];
+                            pomocB += b * macierz[f + k, l + k];
+
+                        }
+                    }
+
+                    pomocR = pomocR / suma;
+                    pomocG = pomocG / suma;
+                    pomocB = pomocB / suma;
+
+
+                    if (pomocR < 0)
+                    {
+                        pomocR = 0;
+                    }
+                    if (pomocG < 0)
+                    {
+                        pomocG = 0;
+                    }
+                    if (pomocB < 0)
+                    {
+                        pomocB = 0;
+                    }
+                    if (pomocR > 255)
+                    {
+                        pomocR = 255;
+                    }
+                    if (pomocG > 255)
+                    {
+                        pomocG = 255;
+                    }
+                    if (pomocB > 255)
+                    {
+                        pomocB = 255;
+                    }
+
+                    bitmapEdited.SetPixel(j, i, Color.FromArgb((int)pomocR, (int)pomocG, (int)pomocB));
+                }
+            }
+
+            pictureBox2.Image = bitmapEdited;
+        
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+                bitmap = (Bitmap)this.pictureBox1.Image;
+
+                wys = pictureBox1.Image.Height;
+                szer = pictureBox1.Image.Width;
+
+                Array.Clear(R, 0, R.Length);
+                Array.Clear(G, 0, G.Length);
+                Array.Clear(B, 0, B.Length);
+
+                histogram();
+
+                K = 0;
+
+                panel2.Invalidate();
+                panel3.Invalidate();
+                pictureBox2.Image = bitmap;
+
+            }
+        }
+    }
+}
